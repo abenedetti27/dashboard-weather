f67b56f60228ba456c14b9c609e0f74ca3661474
abenedetti27 <145300062+abenedetti27@users.noreply.github.com>
2023-10-10 21:25:35 -0700

    add divs for weather info, fixed typos so button renders correctly. added additional variables to display weather data function. updated event listener to click.

--- script.js
+++ script.js
@@ -4,9 +4,10 @@ var cityInput = document.getElementById('city-input');
 var weatherInfo = document.getElementById('weather-info');
 
 //event listener for clicking on submit within form on site
-searchForm.addEventListener('sumbit', function (e){
+searchForm.addEventListener('click', function (e){
     e.preventDefault();
     var cityName = cityInput.value;
+    console.log(cityName)
     getWeatherData(cityName);
     cityInput.value = '';
 }
@@ -16,10 +17,18 @@ searchForm.addEventListener('sumbit', function (e){
 //.then(x => x.text())
 //.then(y => myDisplay(y));
 function getWeatherData(city) {
-    fetch('https://api.openweathermap.org/data/2.5/weather?q='+cityInput+'&appid='+apiKey)
+    fetch('https://api.openweathermap.org/data/2.5/weather?q='+city+'&appid='+apiKey+'&units=imperial')
     .then(response => response.json())
     .then(data => {
+        console.log(data)
         displayWeatherData(data);
+//         lat
+// : 
+// 47.6062
+// lon
+// : 
+// -122.3321
+        getFiveDayForecast(47.6062, -122.3321)//example: data.main.temp; pass two
     })
     .catch(error => {
         console.error('Error:', error);
@@ -28,16 +37,25 @@ function getWeatherData(city) {
 
 //function to display weather data
 function displayWeatherData(data){
-    var cityName = date.name;
-    var temperature = (data.main.temperature);
+    var cityName = data.name;
+    console.log("city name", cityName)
+    var tempEl = document.getElementById('temp');
+    var temperature = data.main.temp; 
+    tempEl.textContent = temperature + ' degrees';
+    console.log("temperature", temperature)
     var weatherDescription = data.weather[0].description;
-
+    console.log("description", weatherDescription)
+    var cityEl = document.getElementById('city');
+    cityEl.textContent = cityName;
+    var descriptionEl = document.getElementById('description');
+    descriptionEl.textContent = weatherDescription;
 
 }
 
+//pass lat and long from data to forecast
 //function to get 5 day forecast
-function getFiveDayForecast(city){
-    fetch('https://api.openweathermap.org/data/2.5/weather?q='+cityInput+'&appid='+apiKey)
+function getFiveDayForecast(lat,lon){
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`)
     .then(response => response.json())
     .then(data => {
         displayFiveDayForecast(data);
@@ -52,7 +70,8 @@ function displayFiveDayForecast(data){
     var cityName = date.name;
     var temperature = (data.main.temperature);
     var weatherDescription = data.weather[0].description;
-
+//needs loop to create boxes
+//console log
 
 }
 
