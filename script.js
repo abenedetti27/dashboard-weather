034983a51d3d83bfd0e9c9b731a94d812c3003ce
abenedetti27 <145300062+abenedetti27@users.noreply.github.com>
2023-10-10 18:00:04 -0700

    work through fetch function

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ var searchForm = document.getElementById('search-form');
 var cityInput = document.getElementById('city-input');
 var weatherInfo = document.getElementById('weather-info');
 
+//event listener for clicking on submit within form on site
 searchForm.addEventListener('sumbit', function (e){
     e.preventDefault();
     var cityName = cityInput.value;
@@ -10,3 +11,21 @@ searchForm.addEventListener('sumbit', function (e){
     cityInput.value = '';
 }
 );
+//function to get weather data using fetch
+//fetch(file)
+//.then(x => x.text())
+//.then(y => myDisplay(y));
+function getWeatherData(city) {
+    fetch('https://api.openweathermap.org/data/2.5/weather?q='+cityInput+'&appid='+apiKey)
+    .then(response => response.json())
+    .then(data => {
+        displayWeatherData(data);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    })
+}
+
+//function to display weather data
+
+//base url: https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
